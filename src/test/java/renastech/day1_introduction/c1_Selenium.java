9f40a4a07bb3e824f50108fa43e1b5aae42605a8
selinbasgz <selinbasgoze2197@gmail.com>
2023-06-20 08:10:08 -0400

    MAIN

--- src/test/java/renastech/day1_introduction/c1_Selenium.java
+++ src/test/java/renastech/day1_introduction/c1_Selenium.java
@@ -1,7 +1,22 @@
 package renastech.day1_introduction;
 
-public class mainselenium {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class c1_Selenium {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world");//My maven still works like a normal java
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+
+        // navigate to facebook on google chrome
+        String facebookURL = "https://www.facebook.com/";
+        // .get method opens a link
+        driver.get(facebookURL);
     }
 }
